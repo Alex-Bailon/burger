e552ea9ed492e310a9ebdf3b074bf4a2aa3d3761
Alex B <abailon949@gmail.com>
2020-04-28 09:39:05 -0700

    updated burger.js to add call back function

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,21 @@
 const orm = require('../config/orm')
 
 var burger = {
-    selectAll: function() {
-      orm.selectAll("burgers");
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      })
     },
-    insertOne: function(options) {
-      orm.insertOne("burgers", options);
+    insertOne: function(options, cb) {
+      orm.insertOne("burgers", options, function(res) {
+        cb(res);
+      });
     },
-    updateOne: function(objColVals, condition) {
-      orm.updateOne("burgers", objColVals, condition);
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
     }
   };
   
-module.exports = cat;
+module.exports = burger;
