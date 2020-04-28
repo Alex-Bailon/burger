8619a84eed5687f383a4409af9e61404a4f233d6
Alex B <abailon949@gmail.com>
2020-04-28 11:40:00 -0700

    added router

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,20 @@
-const mysql = require("mysql");
-​
-const connection = mysql.createConnection({
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
   host: "localhost",
-  // Your username
+  port: 3306,
   user: "root",
-  // Your password
   password: "password",
   database: "burgers_db"
 });
-​
+
 connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
-​
-module.exports = connection;
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const burger = require('../models/burger')
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  let newBurger = `burger_name = '${ req.body.name }'`
+  burger.insertOne( newBurger, function(result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  let condition = "id = " + req.params.id;
+
+  burger.updateOne(' devoured = true ', condition, function(result) {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+module.exports = router;
